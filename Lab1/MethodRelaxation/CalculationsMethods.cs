c328f20f9170a8cccc796a2d66eec46b0ba62371
TI2042 <tis3.1415@gmail.com>
2020-10-21 19:01:42 +0400

    Finished

---
+++ Lab1/MethodRelaxation/CalculationsMethods.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodRelaxation
+{
+    public class CalculationsMethods
+    {
+        private int k = 1;
+        private int colomnX = 0;
+        public void SystemAdaptation(decimal[,] system)
+        {
+
+            for (int i = 0; i < system.GetUpperBound(0) + 1; i++)
+            {
+                decimal temp = system[i, i];
+                for (int j = 0; j < system.GetUpperBound(1) + 1; j++)
+                {
+                    system[i, j] = Math.Round(system[i, j] / temp, 4);
+                }
+            }
+        }
+
+        public void PrintSystem(decimal[,] system)
+        {
+            for (int i = 0; i < system.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < system.GetUpperBound(1) + 1; j++)
+                {
+                    Console.Write($"{system[i, j] + "|",7}");
+
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void Nevyazki(decimal[,] system, decimal[,] nevyazki)
+        {
+            List<decimal> temp = new List<decimal>();
+            for (int i = 0; i < system.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < system.GetUpperBound(1) + 1; j++)
+                {
+                    if (system[i, j] != 1)
+                    {
+                        temp.Add(system[i, j]);
+                    }
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 1; j <= 4; j++)
+                {
+                    if (temp.First() == system[i, 4])
+                    {
+                        nevyazki[i, 0] = temp.First();
+                        temp.Remove(temp.First());
+
+                    }
+                    else
+                    {
+                        nevyazki[i, j] = -temp.First();
+                        temp.Remove(temp.First());
+                    }
+                }
+            }
+        }
+        private void IncrementR(int k, decimal[,] decisionVector, decimal increment)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == colomnX)
+                {
+                    if (decisionVector[k - 1, i] == 0)
+                    {
+                        decisionVector[k, i] = increment;
+                    }
+                    else
+                    {
+                        decisionVector[k, i] = decisionVector[k - 1, i] + increment;
+                    }
+                }
+                else
+                {
+                    decisionVector[k, i] = decisionVector[k - 1, i];
+                }
+            }
+        }
+        private void NewIterNevyazki(decimal[,] nevyazki, decimal[,] decisionVector, decimal[,] iterationNevyzki, double accuracy)
+        {
+            iterationNevyzki[k - 1, 0] = Math.Round(nevyazki[0, 0] - decisionVector[k - 1, 0] + nevyazki[0, 1] * decisionVector[k - 1, 1] + nevyazki[0, 2] * decisionVector[k - 1, 2] + nevyazki[0, 3] * decisionVector[k - 1, 3], (int)accuracy);
+            iterationNevyzki[k - 1, 1] = Math.Round(nevyazki[1, 0] - decisionVector[k - 1, 1] + nevyazki[1, 1] * decisionVector[k - 1, 0] + nevyazki[1, 2] * decisionVector[k - 1, 2] + nevyazki[1, 3] * decisionVector[k - 1, 3], (int)accuracy);
+            iterationNevyzki[k - 1, 2] = Math.Round(nevyazki[2, 0] - decisionVector[k - 1, 2] + nevyazki[2, 1] * decisionVector[k - 1, 0] + nevyazki[2, 2] * decisionVector[k - 1, 1] + nevyazki[2, 3] * decisionVector[k - 1, 3], (int)accuracy);
+            iterationNevyzki[k - 1, 3] = Math.Round(nevyazki[3, 0] - decisionVector[k - 1, 3] + nevyazki[3, 1] * decisionVector[k - 1, 0] + nevyazki[3, 2] * decisionVector[k - 1, 1] + nevyazki[3, 3] * decisionVector[k - 1, 2], (int)accuracy);
+
+        }
+        public bool StopIteration(decimal epsilon, int k, decimal[,] iterationNevyzki)
+        {
+
+            if ((Math.Abs(iterationNevyzki[k - 1, 0]) < epsilon) && (Math.Abs(iterationNevyzki[k - 1, 1]) < epsilon) && (Math.Abs(iterationNevyzki[k - 1, 2]) < epsilon) && (Math.Abs(iterationNevyzki[k - 1, 3]) < epsilon))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public decimal incremet(decimal[,] iterationNevyzki, int k)
+        {
+            decimal max = 0;
+            for (int i = 0; i < 4; i++)
+            {
+
+                if (Math.Abs(iterationNevyzki[k - 1, i]) > Math.Abs(max))
+                {
+                    max = iterationNevyzki[k - 1, i];
+                    colomnX = i;
+                }
+            }
+            return max;
+        }
+        public void Iteration(decimal[,] nevyazki, decimal[,] decisionVector, decimal[,] iterationNevyzki, double accuracy, decimal epsilon)
+        {
+
+            for (int i = 0; i < decisionVector.GetUpperBound(1) + 1; i++)
+            {
+                decisionVector[0, i] = 0;
+            }
+
+            for (int i = 1; i < 14; i++)
+            {
+
+                NewIterNevyazki(nevyazki, decisionVector, iterationNevyzki, accuracy);
+                IncrementR(i, decisionVector, incremet(iterationNevyzki, i));
+                colomnX = 0;
+                if (StopIteration(epsilon, k, iterationNevyzki) == true)
+                {
+                    Finish(decisionVector, iterationNevyzki);
+                    return;
+                }
+                k++;
+            }
+            
+
+        }
+
+        public void Finish(decimal[,] decisionVector, decimal[,] iterationNevyzki)
+        {
+            Console.WriteLine("Итерации");
+            Console.WriteLine("  K\tВектора решений \t\t   Невязки");
+            for (int i = 0; i < k+1; i++)
+            {
+                Console.Write($"{i + 1 + "|",4}");
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"{decisionVector[i, j] + "|",10}");
+                }
+                for (int j = 0; j < 4; j++)
+                {
+                        Console.Write($"{ iterationNevyzki[i, j] + "|",10}");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine($"Вектор решений: {{{decisionVector[k - 1, 0]+";"+ decisionVector[k - 1, 1] + ";" + decisionVector[k - 1, 2] + ";" + decisionVector[k - 1, 3] }}}") ;
+
+        }
+    }
+}
---
+++ Lab1/MethodRelaxation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodRelaxation
+{
+    class Program
+    {
+        public static decimal[,] system = { { 3.2410m, 0.1970m, 0.6430m, 0.2360m, 0.4540m},
+                                    {0.2570m, 3.8530m, 0.3420m, 0.4270m, 0.3710m},
+                                    { 0.3240m, 0.3170m, 2.7930m, 0.2380m, 0.4650m},
+                                    { 0.4380m, 0.3260m, 0.4830m, 4.2290m, 0.8220m}};
+
+        public static decimal[,] nevyzki = new decimal [4,4];
+        public static decimal[,] decisionVector = new decimal[15,4];
+        public static decimal[,] iterationNevyzki = new decimal[15, 4];
+        public static decimal epsilon; 
+        public static double accuracy;
+        static void Main(string[] args)
+        {
+            Console.Write("Введите точность вычислений:");
+            string strE = Console.ReadLine();
+            try
+            {
+                epsilon = Convert.ToDecimal(strE);
+            }
+            catch
+            {
+                strE = strE.Replace(".", ",");
+                epsilon = Convert.ToDecimal(strE);
+            }
+            accuracy = Math.Abs(Math.Log10((double)epsilon)) + 1;
+            Console.WriteLine("Эпcилон равен :"+epsilon+"\n" +
+                "Точность вычислений равна "+ (accuracy) + " знакам после запятой");
+
+            
+            Console.WriteLine("Преобразование системы \n" +
+                "матрица А \t\t   вектор B");
+            CalculationsMethods calculations = new CalculationsMethods();
+            calculations.SystemAdaptation(system);
+            calculations.PrintSystem(system);
+            Console.WriteLine("Невязки ");
+            calculations.Nevyazki(system, nevyzki);
+            calculations.PrintSystem(nevyzki);
+            Console.WriteLine(" \n" );
+            calculations.Iteration(nevyzki, decisionVector, iterationNevyzki, accuracy ,  epsilon);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
