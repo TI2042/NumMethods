4bd17704a9d6961047df29a017f7522aeb8d2641
TI2042 <tis3.1415@gmail.com>
2020-10-26 14:10:11 +0400

    Lab 3 is finished

---
+++ Lab1/MethodLotsmana/CalculationMethods.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodLotsmana
+{
+    public class CalculationMethods
+    {
+        public decimal Function(decimal x, double accuracy)
+        {
+            decimal s = -0.5m * x * x;
+            decimal function = x * (decimal)Math.Log((double)x) - (decimal)Math.Exp((double)s);
+            return Math.Round(function, (int)accuracy);
+        }
+        public decimal FirstDerivative(decimal x, double accuracy)
+        {
+            decimal s = -0.5m * x * x;
+            decimal derivative = Decimal.Add((decimal)Math.Log((double)x), decimal.Multiply(x, (decimal)Math.Exp((double)s))) + 1;
+            return Math.Round(derivative, (int)accuracy);
+        }
+
+        public decimal SecondDerivative(decimal x, double accuracy)
+        {
+            decimal s = -0.5m * x * x;
+            decimal derivative = 1 / x + (decimal)Math.Exp((double)s) + decimal.Multiply(decimal.Multiply(x, (decimal)Math.Exp((double)s)), -x);
+            return Math.Round(derivative, (int)accuracy);
+        }
+
+        public decimal IterativeProcess(decimal x, double accuracy)
+        {
+            decimal newX = x - (2 * Function(x, accuracy) * FirstDerivative(x, accuracy)) / (2 * (decimal)Math.Pow((double)FirstDerivative(x, accuracy), 2) - Function(x, accuracy) * SecondDerivative(x, accuracy));
+            return (decimal)Math.Round((double)newX, (int)accuracy);
+        }
+
+        public void Iteration(decimal x0, double accuracy, decimal epsilon)
+        {
+            decimal x = x0;
+            int n = 1;
+            decimal condition;
+            decimal newX = IterativeProcess(x, accuracy);
+            Console.WriteLine($"{" n|",3} {" X|",10} {" Xn+1|",10} {"|Xn-Xn-1||",10}");
+            Console.WriteLine($"{n-1 + "|",3} {x + "|",10} {newX + "|",10} ");
+            do
+            {
+                condition = Math.Abs(newX - x);
+                x = newX;
+                newX = IterativeProcess(x, accuracy);
+                Console.WriteLine($"{n + "|",3} {x + "|",10} {newX + "|",10} {condition + "|",10}");
+                n++;
+            } while (Math.Abs(x - newX) >= epsilon);
+
+            condition = Math.Abs(newX - x);
+            x = newX;
+            newX = IterativeProcess(x, accuracy);
+            Console.WriteLine($"{n + "|",3} {x + "|",10} {newX + "|",10} {condition + "|",10}");
+        }
+    }
+}
---
+++ Lab1/MethodLotsmana/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodLotsmana
+{
+    class Program
+    {
+        public static decimal[,] system = { { 3.2410m, 0.1970m, 0.6430m, 0.2360m, 0.4540m},
+                                    {0.2570m, 3.8530m, 0.3420m, 0.4270m, 0.3710m},
+                                    { 0.3240m, 0.3170m, 2.7930m, 0.2380m, 0.4650m},
+                                    { 0.4380m, 0.3260m, 0.4830m, 4.2290m, 0.8220m}};
+
+        public static decimal[,] nevyzki = new decimal[4, 4];
+        public static decimal[,] decisionVector = new decimal[300, 4];
+        public static decimal[,] iterationNevyzki = new decimal[300, 4];
+        public static decimal epsilon;
+        public static double accuracy;
+        public static decimal x0;
+        static void Main(string[] args)
+        {
+            CalculationMethods calculations = new CalculationMethods();
+            Console.Write("Введите Начально приближение:");
+            string strX = Console.ReadLine();
+            try
+            {
+                x0 = Convert.ToDecimal(strX);
+            }
+            catch
+            {
+                strX = strX.Replace(".", ",");
+                x0 = Convert.ToDecimal(strX);
+            }
+            Console.Write("Введите Эпсилон:");
+            string strE = Console.ReadLine();
+            try
+            {
+                epsilon = Convert.ToDecimal(strE);
+            }
+            catch
+            {
+                strE = strE.Replace(".", ",");
+                epsilon = Convert.ToDecimal(strE);
+            }
+            accuracy = Math.Abs(Math.Log10((double)epsilon)) + 1;
+            Console.WriteLine("Начальное приближение:"+ x0+"\n"+" Эпcилон равен :" + epsilon + "\n" +
+                "Точность вычислений равна " + (accuracy) + " знакам после запятой");
+            
+            calculations.Iteration(x0, accuracy, epsilon);
+            Console.ReadKey();
+        }
+
+    }
+}
